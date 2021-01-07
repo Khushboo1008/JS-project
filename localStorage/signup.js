3f2bf0ec1eaeaf908b184595ef0883f75883609d
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-01-07 19:57:17 +0530

    sign up button

--- localStorage/signup.js
+++ localStorage/signup.js
@@ -1,10 +1,12 @@
 function submitForm() {
+  let name = document.getElementById("name").value;
+  localStorage.setItem("name", name);
   let username = document.getElementById("username").value;
   localStorage.setItem("username", username);
+  let email = document.getElementById("email").value;
+  localStorage.setItem("email", email);
   let password = document.getElementById("password").value;
   localStorage.setItem("password", password);
   let confirmPassword = document.getElementById("confirm-password").value;
   localStorage.setItem("confirm-password", confirmPassword);
-  let email = document.getElementById("email").value;
-  localStorage.setItem("email", email);
 }
