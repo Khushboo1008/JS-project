3bd089650b4d019484b92185f5820c0050811bd3
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-01-07 19:41:54 +0530

    signup.js

--- localStorage/login.js
+++ localStorage/login.js
@@ -1,6 +1,6 @@
 function myFunc() {
-  let c = document.getElementById("username").value;
-  localStorage.setItem("username", c);
-  let p = document.getElementById("password").value;
-  localStorage.setItem("password", p);
+  let username = document.getElementById("username").value;
+  localStorage.setItem("username", username);
+  let password = document.getElementById("password").value;
+  localStorage.setItem("password", password);
 }
--- localStorage/signup.js
+++ localStorage/signup.js
@@ -1,1 +1,10 @@
-function 
+function submitForm() {
+  let username = document.getElementById("username").value;
+  localStorage.setItem("username", username);
+  let password = document.getElementById("password").value;
+  localStorage.setItem("password", password);
+  let confirmPassword = document.getElementById("confirm-password").value;
+  localStorage.setItem("confirm-password", confirmPassword);
+  let email = document.getElementById("email").value;
+  localStorage.setItem("email", email);
+}
