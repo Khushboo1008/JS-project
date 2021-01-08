40fd0757870ba123d73b88098005c060d775fa15
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-01-09 02:12:23 +0530

    signup object formation done

--- localStorage/signup.js
+++ localStorage/signup.js
@@ -1,12 +1,10 @@
 function submitForm() {
-  let name = document.getElementById("name").value;
-  localStorage.setItem("name", name);
-  let username = document.getElementById("username").value;
-  localStorage.setItem("username", username);
-  let email = document.getElementById("email").value;
-  localStorage.setItem("email", email);
-  let password = document.getElementById("password").value;
-  localStorage.setItem("password", password);
-  let confirmPassword = document.getElementById("confirm-password").value;
-  localStorage.setItem("confirm-password", confirmPassword);
+  var myObj={
+    username: document.getElementById("username").value,
+    email: document.getElementById("email").value,
+    name: document.getElementById("name").value,
+    password: document.getElementById("password").value,
+    confirmPassword: document.getElementById("confirm-password").value,
+  }
+  localStorage.setItem('allUserDetails', JSON.stringify(myObj));
 }
