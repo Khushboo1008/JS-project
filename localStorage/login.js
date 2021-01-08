5d127c621a768d56936c9dc7974976cb4bfba895
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-01-09 02:14:00 +0530

    login object formation done

--- localStorage/login.js
+++ localStorage/login.js
@@ -1,6 +1,7 @@
 function myFunc() {
-  let username = document.getElementById("username").value;
-  localStorage.setItem("username", username);
-  let password = document.getElementById("password").value;
-  localStorage.setItem("password", password);
+  var myObj={
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  }
+  localStorage.setItem('allUserDetails', JSON.stringify(myObj));
 }
