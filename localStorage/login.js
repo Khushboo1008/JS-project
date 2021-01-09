64fb481970c32a99b887847c70af16d89e2a14e4
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-01-10 01:08:46 +0530

    css set

--- localStorage/login.js
+++ localStorage/login.js
@@ -1,10 +1,4 @@
 function myFunc() {
-  // var Obj={
-  //   username: document.getElementById("username").value,
-  //   password: document.getElementById("password").value,
-  // }
-  // localStorage.setItem('allUserDetails', JSON.stringify(Obj));
-  // https://www.c-sharpcorner.com/blogs/store-multiple-values-in-localstorage-apin-in-html-5
   username = document.getElementById("username").value;
   password = document.getElementById("password").value;
 
@@ -19,8 +13,8 @@ function addNewObject(username, password) {
     user: username,
     pass: password,
   };
+
   allUserDetails.push(currentUser);
-  // showList = showList.concat(JSON.parse(localStorage.getItem("showList") || "[]"));
-  console.log(allUserDetails);
+
   localStorage.setItem("allUserDetails", JSON.stringify(allUserDetails));
 }
