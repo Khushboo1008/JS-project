6d092c73b175ebea65d1916a63e53a12109e7450
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-01-09 20:43:22 +0530

    login string formed

--- localStorage/login.js
+++ localStorage/login.js
@@ -1,7 +1,29 @@
+
 function myFunc() {
-  var myObj={
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-  }
-  localStorage.setItem('allUserDetails', JSON.stringify(myObj));
+  // var Obj={
+  //   username: document.getElementById("username").value,
+  //   password: document.getElementById("password").value,
+  // }
+  // localStorage.setItem('allUserDetails', JSON.stringify(Obj));
+  // https://www.c-sharpcorner.com/blogs/store-multiple-values-in-localstorage-apin-in-html-5
+
+
+
+
+  username= document.getElementById("username").value;
+  password= document.getElementById("password").value;
+
+  addNewObject(username,password);
+}
+function addNewObject(username, password){
+  let allUserDetails = JSON.parse(localStorage.getItem("allUserDetails") || "[]");
+    
+  let show = {
+    user: username,
+    pass: password,
+  };
+  allUserDetails.push(show);
+  // showList = showList.concat(JSON.parse(localStorage.getItem("showList") || "[]"));
+  console.log(allUserDetails);
+  localStorage.setItem("allUserDetails", JSON.stringify(allUserDetails));
 }
