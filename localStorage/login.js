8e53f2d2cfe67feb1949f0a56d93130f49afda87
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-01-09 20:43:56 +0530

    few more

--- localStorage/login.js
+++ localStorage/login.js
@@ -6,10 +6,6 @@ function myFunc() {
   // }
   // localStorage.setItem('allUserDetails', JSON.stringify(Obj));
   // https://www.c-sharpcorner.com/blogs/store-multiple-values-in-localstorage-apin-in-html-5
-
-
-
-
   username= document.getElementById("username").value;
   password= document.getElementById("password").value;
 
@@ -18,11 +14,11 @@ function myFunc() {
 function addNewObject(username, password){
   let allUserDetails = JSON.parse(localStorage.getItem("allUserDetails") || "[]");
     
-  let show = {
+  let currentUser = {
     user: username,
     pass: password,
   };
-  allUserDetails.push(show);
+  allUserDetails.push(currentUser);
   // showList = showList.concat(JSON.parse(localStorage.getItem("showList") || "[]"));
   console.log(allUserDetails);
   localStorage.setItem("allUserDetails", JSON.stringify(allUserDetails));
