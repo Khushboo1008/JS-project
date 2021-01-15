262d7cc5c8c98f9b7802e590bb41b59db8749bad
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-01-15 23:08:51 +0530

    body changed

--- localStorage/js/dashboard.js
+++ localStorage/js/dashboard.js
@@ -1,6 +1,8 @@
-var person = localStorage.getItem("allUserDetails");
-console.log("hello" + person);
-document.getElementById("greet").innerHTML = "person";
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed!";
-}
+// var person = localStorage.getItem("allUserDetails");
+// console.log("hello" + person);
+// document.getElementById("greet").innerHTML = "person";
+// if (document.getElementById("greet").innerHTML == "Hello ")
+//   document.getElementById("greet").innerHTML = "Paragraph changed!";
+// function hello() {
+document.getElementById("greet").innerText = "Welcome girl";
+// }
