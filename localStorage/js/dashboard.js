28cf23fea79c902b774ec560603ddad54aaa0dbf
Khush2809 <2019.khushboo.dhingra@ves.ac.in>
2021-01-15 22:23:02 +0530

    try on greetings in dashboard

---
+++ localStorage/js/dashboard.js
@@ -0,0 +1,6 @@
+var person = localStorage.getItem("allUserDetails");
+console.log("hello" + person);
+document.getElementById("greet").innerHTML = "person";
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed!";
+}
