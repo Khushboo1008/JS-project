d55585629598ea0e8aa2ad723c68769e5fb90962
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-01-15 23:20:37 +0530

    logout done

--- localStorage/js/dashboard.js
+++ localStorage/js/dashboard.js
@@ -1,8 +1,6 @@
 // var person = localStorage.getItem("allUserDetails");
 // console.log("hello" + person);
-// document.getElementById("greet").innerHTML = "person";
-// if (document.getElementById("greet").innerHTML == "Hello ")
-//   document.getElementById("greet").innerHTML = "Paragraph changed!";
-// function hello() {
 document.getElementById("greet").innerText = "Welcome girl";
-// }
+function logout() {
+  window.location.href = "login.html";
+}
